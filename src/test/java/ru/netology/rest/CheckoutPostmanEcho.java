97e8c8c3e379cedb5ecf443a40dfe5dd7caa64d0
Max Shvagerus <mshvagerus@gmail.com>
2023-01-26 08:11:23 +0300

    changing test

--- src/test/java/ru/netology/rest/CheckoutPostmanEcho.java
+++ src/test/java/ru/netology/rest/CheckoutPostmanEcho.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class CheckoutPostmanEcho {
@@ -14,14 +15,14 @@ public class CheckoutPostmanEcho {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Still Loving You - Scorpions (Скорпионы)") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("date", equalTo("Still Loving You - Queen (Скорпионы)"))
         ;
     }
 }
