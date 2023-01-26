06c89b7910ceb3e1041853d6d2a561ccce4b519f
Max Shvagerus <mshvagerus@gmail.com>
2023-01-26 07:29:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/rw8x3yt89vtbxk2s?svg=true)](https://ci.appveyor.com/project/shvisor/autotesting-2-1-rest)
---
+++ src/test/java/ru/netology/rest/CheckoutPostmanEcho.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class CheckoutPostmanEcho {
+    @Test
+    void shouldCheckoutPostmanEcho() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
