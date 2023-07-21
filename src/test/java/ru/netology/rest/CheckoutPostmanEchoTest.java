1f4ce9af4a053f3fd4351d4727234f11ae19fb5f
Max Shvagerus <mshvagerus@gmail.com>
2023-07-21 22:46:58 +0300

    reformat code

--- src/test/java/ru/netology/rest/CheckoutPostmanEchoTest.java
+++ src/test/java/ru/netology/rest/CheckoutPostmanEchoTest.java
@@ -11,16 +11,16 @@ public class CheckoutPostmanEchoTest {
         // Given - When - Then
         // Предусловия
         given()
-                .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("Still Loving You - Scorpions (Скорпионы)") // отправляемые данные (заголовки и query можно выставлять аналогично)
+            .baseUri("https://postman-echo.com")
+            .contentType("text/plain; charset=UTF-8")
+            .body("Still Loving You - Scorpions (Скорпионы)") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-                .when()
-                .post("/post")
+        .when()
+            .post("/post")
 // Проверки
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("Still Loving You - Scorpions (Скорпионы)"))
+        .then()
+            .statusCode(200)
+            .body("data", equalTo("Still Loving You - Scorpions (Скорпионы)"))
         ;
     }
 }
